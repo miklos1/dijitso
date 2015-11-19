8ebbdf4f7b13da7d417afec41b2776203d20213a
Martin Sandve Alnæs <martinal@simula.no>
2015-11-20 00:57:23 +0100

    Make default runtests.sh script not run forever.

--- test/runtests.sh
+++ test/runtests.sh
@@ -4,10 +4,7 @@ set -e exit
 echo RUNNING SERIAL
 py.test
 
-while [ $? -eq 0 ]
-do
-  for p in 1 4 8; do
-    echo RUNNING WITH p=$p
-    mpirun -n $p python -B -m pytest -svl
-  done
+for p in 1 4 8; do
+  echo RUNNING WITH p=$p
+  mpirun -n $p python -B -m pytest -svl
 done
