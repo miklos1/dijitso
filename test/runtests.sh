d1e91629e1b79f014c70ab553ef5bca87d500045
Martin Sandve Alnæs <martinal@simula.no>
2015-11-20 09:28:19 +0100

    Try 16 processes in runtests.sh.

--- test/runtests.sh
+++ test/runtests.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 set -e exit
 
-echo RUNNING SERIAL
+echo Running tests in serial:
 py.test
 
-for p in 1 4 8; do
-  echo RUNNING WITH p=$p
+for p in 1 4 8 16; do
+  echo Running tests with mpi, n=$p
   mpirun -n $p python -B -m pytest -svl
 done
