d5c3f9b086f9cbe5074858627eedb2556164c891
Martin Sandve Alnæs <martinal@simula.no>
2016-03-09 12:20:21 +0100

    Make coverage reports write to different directories for each process under mpi.

--- test/runtests.sh
+++ test/runtests.sh
@@ -2,9 +2,12 @@
 set -e exit
 
 echo Running tests in serial:
-py.test
+export MYRANK=serial
+export MPISIZE=0
+python -B -m pytest -svl --cov-report html --cov=dijitso
 
 for p in 1 4 8 16; do
   echo Running tests with mpi, n=$p
-  mpirun -n $p ./mpipipe.sh python -B -m pytest -svl
+  export MPISIZE=$p
+  mpirun -n $p ./mpipytest.sh
 done
