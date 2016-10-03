8e1345556cb70fa8f11ae231a4c30b144039904c
Martin Sandve Alnæs <martinal@simula.no>
2016-10-03 10:40:28 +0200

    Use environment variable  if set in test bash scripts to fix issue 18.

--- test/mpipipe.sh
+++ test/mpipipe.sh
@@ -20,13 +20,15 @@
 
 # This script redirects output from a command to output.MYRANK
 
+: ${PYTHON:=python}
+
 # See: http://docs.roguewave.com/threadspotter/2012.1/linux/manual_html/apas03.html
 if [ ! -z $OMPI_COMM_WORLD_RANK ]; then
     MYRANK=$OMPI_COMM_WORLD_RANK
 elif [ ! -z $PMI_RANK ]; then
     MYRANK=$PMI_RANK
 else
-    MYRANK=`python -c "import dolfin;c=dolfin.mpi_comm_world();r=dolfin.MPI.rank(c);print(r);dolfin.MPI.barrier(c)"`
+    MYRANK=`${PYTHON} -c "import dolfin;c=dolfin.mpi_comm_world();r=dolfin.MPI.rank(c);print(r);dolfin.MPI.barrier(c)"`
 fi
 
 OUT=output.$MYRANK
--- test/runtests.sh
+++ test/runtests.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 set -e exit
 
+: ${PYTHON:=python}
+
 echo Running tests in serial:
 export MYRANK=
 export MPISIZE=
-python -B -m pytest -svl --cov-report html --cov=dijitso --junitxml report$MYRANK.xml
+${PYTHON} -B -m pytest -svl --cov-report html --cov=dijitso --junitxml report$MYRANK.xml
 
 for p in 1 4 8 16; do
   echo Running tests with mpi, n=$p
